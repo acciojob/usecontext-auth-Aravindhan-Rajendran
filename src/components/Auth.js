b5a3ba92d9b839c5b1c5ab9f3ee7a2e02fa8bd99
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-01 16:16:35 +0530

    Update Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAuth } from './AuthContext';
 
 const Auth = () => {
-  const { isAuthenticated, authenticate } = useAuth();
+  const { isAuthenticated, authenticate, logout } = useAuth();
 
   return (
     <div>
