afb1f71ba88f9a259779d119dbe0c51ef4826c2d
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-01 16:19:37 +0530

    Update Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,8 +1,9 @@
+// src/Auth.js
 import React from 'react';
 import { useAuth } from './AuthContext';
 
 const Auth = () => {
-  const { isAuthenticated, authenticate, logout } = useAuth();
+  const { isAuthenticated, authenticate } = useAuth();
 
   return (
     <div>
@@ -10,11 +11,11 @@ const Auth = () => {
         {isAuthenticated ? "You are now authenticated, you can proceed" : "You are not authenticated"}
       </p>
       <div>
-<input
-  type="checkbox"
-  onChange={(e) => e.target.checked ? authenticate() : logout()}
-  id="auth-checkbox"
-/>
+        <input
+          type="checkbox"
+          onChange={authenticate}
+          id="auth-checkbox"
+        />
         <label htmlFor="auth-checkbox">I'm not a robot</label>
       </div>
     </div>
