887e582ac21b20b0bd1c21dff839321a678c999f
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-01 16:12:04 +0530

    Update Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -10,11 +10,11 @@ const Auth = () => {
         {isAuthenticated ? "You are now authenticated, you can proceed" : "You are not authenticated"}
       </p>
       <div>
-        <input
-          type="checkbox"
-          onChange={authenticate}
-          id="auth-checkbox"
-        />
+<input
+  type="checkbox"
+  onChange={(e) => e.target.checked ? authenticate() : logout()}
+  id="auth-checkbox"
+/>
         <label htmlFor="auth-checkbox">I'm not a robot</label>
       </div>
     </div>
