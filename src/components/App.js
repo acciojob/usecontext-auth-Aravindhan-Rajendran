c6c6d38928093a01bb7741206c14013ae97ee7fa
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-01 15:46:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-
-import React from "react";
+// src/App.js
+import React from 'react';
+import { AuthProvider, useAuth } from './AuthContext';
 import './../styles/App.css';
+
+const Auth = () => {
+  const { isAuthenticated, authenticate } = useAuth();
+
+  return (
+    <div>
+      <h1>{isAuthenticated ? "You are authenticated!" : "Click on the checkbox to get authenticated"}</h1>
+      <div>
+        <input
+          type="checkbox"
+          onChange={authenticate}
+          id="auth-checkbox"
+        />
+        <label htmlFor="auth-checkbox">I'm not a robot</label>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <AuthProvider>
+      <Auth />
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
